164b3f78866274bce8a3cea0a5a3b54884d1237a
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-19 13:51:11 +0100

    Update gui look

--- src/main/java/com/sample/DialogGUI.java
+++ src/main/java/com/sample/DialogGUI.java
@@ -1,12 +1,14 @@
 package com.sample;
 
 import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import javax.swing.UIManager;
 import javax.swing.plaf.FontUIResource;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Color;
 
 public class DialogGUI implements GUI {
 	private String title = "Question";
@@ -14,19 +16,22 @@ public class DialogGUI implements GUI {
 	public DialogGUI() {
 		UIManager.put("OptionPane.minimumSize", new Dimension(400, 150));
 		UIManager.put("OptionPane.buttonFont", new FontUIResource(new Font("Arial", Font.PLAIN, 20))); 
+		UIManager.put("OptionPane.background", new Color(245, 245, 245));
+		UIManager.put("Panel.background", new Color(245, 245, 245));
+		UIManager.put("Button.background", new Color(255, 255, 255));
+		UIManager.put("Button.foreground", Color.black);
 	}
 
 	@Override
 	public String askQuestion(String question, String[] possibleAnswers) {
-		JLabel label = new JLabel(question);
-		label.setFont(new Font("Arial", Font.PLAIN, 25));
+		JLabel label = createLabel(question);
 		
 		int answerIndex = JOptionPane.showOptionDialog(
 				null,
 				label,
 				title,
 				JOptionPane.DEFAULT_OPTION,
-				JOptionPane.QUESTION_MESSAGE,
+				JOptionPane.PLAIN_MESSAGE,
 				null,
 				possibleAnswers,
 				possibleAnswers[0]);
@@ -36,9 +41,15 @@ public class DialogGUI implements GUI {
 
 	@Override
 	public void showResult(String result) {
-		JLabel label = new JLabel(result);
+		JLabel label = createLabel("Your musical instrument: " + result);
+		JOptionPane.showMessageDialog(null, label, "Result", JOptionPane.PLAIN_MESSAGE, null);
+	}
+	
+	private JLabel createLabel(String message) {
+		JLabel label = new JLabel(message);
 		label.setFont(new Font("Arial", Font.PLAIN, 25));
-		JOptionPane.showMessageDialog(null, label);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		return label;
 	}
 
 }
